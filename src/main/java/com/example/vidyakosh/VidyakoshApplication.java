7ccb8e9d0390c77300133a18e2659714ab75bab5
Siddharth-Lokhande <sidlokhande2504@gmail.com>
2025-09-24 17:18:09 +0530

    1

---
+++ src/main/java/com/example/vidyakosh/VidyakoshApplication.java
@@ -0,0 +1,13 @@
+package com.example.vidyakosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidyakoshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidyakoshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vidyakosh/VidyakoshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vidyakosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidyakoshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
